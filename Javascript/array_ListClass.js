51579499ddc53211cb779a44f2d205fed2b135fe
nguyenthiti2505 <you@example.com>
2020-02-17 17:25:28 +0700

    listclassmember

---
+++ Javascript/array_ListClass.js
@@ -0,0 +1,5 @@
+var Listclass = [{name: 'Bui Thi Thao', age: 21, isFemale : true},
+				 {name: 'Nguyen Thi Nhi', age: 21, isFemale : true},
+				 {name: 'Nguyen Duc Huy', age: 22, isFemale : false},
+				]
+console.log(Listclass(0));
